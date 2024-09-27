257eccc38c6bd4e041930b7c261810676a0b2e66
bkady <btk1015@unh.edu>
2024-09-27 14:19:00 -0400

    output to csv fule and untrack julie's script

--- src/script.py
+++ src/script.py
@@ -23,6 +23,9 @@ def select_and_read_file(file_path):
                 # Reshape the data into the desired matrix
                 F = int32_data.reshape(10, 120000)
 
+                # Write output to txt file
+                np.savetxt("../output/F_Matrix.csv", F, delimiter=",")
+
                 print(F)
                 
         except Exception as e:
