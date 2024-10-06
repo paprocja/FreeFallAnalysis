77728fcd5b611a70a785e32905c3a83cc765d7fd
Austin Snow <acs1114@usnh.edu>
2024-10-06 19:37:15 -0400

    Removed alternative file upload (both types can be selected from the same button), added blueDrop id number dropdown menu

--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
-import FileReader
 import FileSelectUI
 from BD_Data import BD_Data
 
-# F array from mat lab script
-# look into making this not a global variable but it may be a necessity because IDK how the UI command can return
+# TODO look into making this not a global variable but it may be a necessity because IDK how the UI command can return
 # a value
+# BD_Data object with F array from mat lab script and g sensor values
 bd_data = None
 
 ## UI COMMANDS
 
 # This is a command that is passed into FileSelectUI to be executed on a button press
-def on_select_file(file_path):
+def on_select_file(file_path, bdid):
     global bd_data
-    bd_data = BD_Data(file_path)
+    bd_data = BD_Data(file_path, bdid)
     
-
 # saves a numpy array as a csv file
 # may make more sense to go somewhere else at some point 
 def save_to_csv():
