cb071cc182b4ce5e957acd4d36a5c4c87fb1ae21
brc1051 <boyd.crapo@unh.edu>
2025-03-26 11:37:12 -0400

    Annotations and scatter points

--- src/UI/FigureManager.py
+++ src/UI/FigureManager.py
@@ -171,6 +171,88 @@ class FigureManager:
         self.validation_types[label] = validation_type  # Store validation function
         self.valid_inputs[label] = False  # Mark as not valid initially
 
+    
+    def find_label_partner(self, label):
+        """
+        Finds the corresponding partner label for a given label,
+             if their is a pair of them
+        
+        Paramters
+        ---------
+        label: string
+            The label that we want to use to find the other paired label
+
+        Return: the other label
+        """
+        pair_mapping = {
+        'Enter Start Time: ': 'Enter End Time: ',
+        'Enter End Time: ': 'Enter Start Time: ',
+        'Enter Pressure Start: ': 'Enter Pressure End: ',
+        'Enter Pressure End: ': 'Enter Pressure Start: ',
+        'Enter Profile Increase: ': 'Enter Profile Decrease: ',
+        'Enter Profile Decrease: ': 'Enter Profile Increase: '
+        }
+        return pair_mapping.get(label, "")
+
+    def validate_input(self, label, text):
+        """
+        Validates the input based on the type and updates the 
+            corresponding start or end vale
+
+        Parameters
+        ----------
+        label: string
+            The name of the textbox being run
+        text: string
+            The info currently in the textbox
+        """
+
+        # set the validation data for this input
+        self.text_values[label] = text
+        validator_type = self.validation_types[label]
+        self.validator.set_type(validator_type)
+
+        if validator_type in ['t_start', 't_end', 'p_start', 'p_end', 'p_inc', 'p_dec']:
+            # Grab the label that is paired to this input type and get the stored input from that label
+            paired_label = self.find_label_partner(label)
+            paired_text = self.text_values.get(paired_label, "")
+
+            # Send the data points to the validator
+            if validator_type in ['t_start', 'p_start', 'p_inc']:
+                is_valid = self.validator.validate(text, paired_text)
+            else:
+                is_valid = self.validator.validate(paired_text, text)
+
+            # If the data was validated plot the points and annotate them on the figure
+            if is_valid and validator_type in ['t_start', 't_end']:
+                    self.remove_points()
+                    self.plot_point(text, self.validator.time_range[int(text)])
+                    self.plot_point(paired_text, self.validator.time_range[int(paired_text)])
+            elif is_valid and validator_type in ['p_start', 'p_end']:
+                    self.remove_points()
+                    self.plot_point(text, self.validator.penetrometer_data.ppm[int(text)])
+                    self.plot_point(paired_text, self.validator.penetrometer_data.ppm[int(paired_text)])
+            elif is_valid and validator_type in ['p_inc', 'p_dec']:
+                    self.remove_points()
+                    self.plot_point(text, self.validator.pore_pressure.deceleration_profile[int(text)])
+                    self.plot_point(paired_text, self.validator.pore_pressure.deceleration_profile[int(paired_text)])
+
+            # Set if the input was valid for the two input values from the ranges
+            self.valid_inputs[label] = is_valid
+            self.valid_inputs[paired_label] = is_valid
+            return is_valid
+        else:
+
+            if text == "": # Need this check if the user 'submits' nothing in the text box
+                self.valid_inputs[label] = False
+                return False
+            if self.validator.validate(text): # Check if the input text is valid
+                self.valid_inputs[label] = True
+                return True
+            else:
+                self.valid_inputs[label] = False
+                return False
+    
     def store_text(self, label, validator, text):
         """ 
         Stores input, validates it, and updates status. 
@@ -186,77 +268,12 @@ class FigureManager:
         
         """
         self.text_values[label] = text
-        validator.set_type(self.validation_types[label])
-        # Apply the validation rule using the validator instance. 
-        #   Special cases for anything with multiple inputs.
-        if validator.type == 'end':
-            self.end = text
-            if validator.validate(self.start, text):  
-                self.valid_inputs[label] = True
-                self.valid_inputs["Enter Start Time: "] = True
-                self.end = text
-                self.remove_invalid_text()
-            else:
-                self.valid_inputs[label] = False
-                self.add_invalid_text()
-        elif validator.type == 'start':
-            self.start = text
-            if validator.validate(text, self.end):
-                self.valid_inputs[label] = True
-                self.valid_inputs["Enter End Time: "] = True
-                self.start = text
-                self.remove_invalid_text()
-            else:
-                self.valid_inputs[label] = False
-                self.add_invalid_text()
-        elif validator.type == 'p_end':
-            self.p_end = text
-            if validator.validate(self.p_start, text):
-                self.valid_inputs[label] = True
-                self.valid_inputs["Enter Pressure Start: "] = True
-                self.p_end = text
-                self.remove_invalid_text()
-            else:
-                self.valid_inputs[label] = False
-                self.add_invalid_text()
-        elif validator.type == 'p_start':
-            self.p_start = text
-            if validator.validate(text, self.p_end):
-                self.valid_inputs[label] = True
-                self.valid_inputs["Enter Pressure End: "] = True
-                self.p_start = text
-                self.remove_invalid_text()
-            else:
-                self.valid_inputs[label] = False
-                self.add_invalid_text()
-        elif validator.type == 'p_dec':
-            self.p_dec = text
-            if validator.validate(self.p_inc, text):
-                self.valid_inputs[label] = True
-                self.valid_inputs["Enter Profile Increase: "] = True
-                self.p_dec = text
-                self.remove_invalid_text()
-            else:
-                self.valid_inputs[label] = False
-                self.add_invalid_text()
-        elif validator.type == 'p_inc':
-            self.p_inc = text
-            if validator.validate(text, self.p_dec): 
-                self.valid_inputs[label] = True
-                self.valid_inputs["Enter Profile Decrease: "] = True
-                self.p_inc = text
-                self.remove_invalid_text()
-            else:
-                self.valid_inputs[label] = False
-                self.add_invalid_text()
-        else:
-            if validator.validate(text):  
-                self.valid_inputs[label] = True
-                self.remove_invalid_text()
-            
-            else:
-                self.valid_inputs[label] = False
-                self.add_invalid_text()
+        if self.validate_input(label, text):
+            # self.valid_inputs[label] = True
+            self.remove_invalid_text()
+        else: 
+            # self.valid_inputs[label] = False
+            self.add_invalid_text()
 
     def add_radio(self, position):
         """
@@ -279,8 +296,6 @@ class FigureManager:
         radio.on_clicked(self.yes_no_from_radio)
         self.radio_buttons.append(radio)
 
-
-
     def yes_no_from_radio(self, label):
         """
         sets the result of the radio button based on what value is clicked.
@@ -295,8 +310,6 @@ class FigureManager:
         else:
             self.radio_result = False
             
-
-  
     def wait_for_valid_inputs(self):
         """
         Waits until all text boxes contain valid values.
@@ -312,7 +325,7 @@ class FigureManager:
     
     def add_info_text(self, text, x_off, y_off, width):
         """
-        Adds a default axes to be used to but text into
+        Adds a new info text box to the figure
 
         Parameters
         ----------
@@ -326,23 +339,26 @@ class FigureManager:
             The width of the widget for the information to be but into
         """
 
-        if hasattr(self, 'info_ax'):
-            self.info_ax.remove()
-        self.info_ax = self.fig.add_axes([x_off, y_off, width, 0.05], facecolor='lightgrey')
-        self.info_ax.set_xticks([])
-        self.info_ax.set_yticks([])
-        self.info_text = self.info_ax.text(0.5, 0.5, text, 
-                                           ha='center', va='center',
-                                           color='black', fontsize=12)
-        
+        if not hasattr(self, 'info_axes'):
+            self.info_axes= []
+
+        ax = self.fig.add_axes([x_off, y_off, width, 0.05], facecolor='lightgrey')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.text(0.5, 0.5, text, 
+                ha='center', va='center',
+            color='black', fontsize=12)
+        self.info_axes.append(ax)
+
     def remove_info_text(self):
         """
-        Removes any instances of info_text widgets currently in the figure
+        Removes all instance of info text boxes from the figure.
         """
 
-        if hasattr(self, 'info_ax'):
-            self.info_ax.remove()
-            del self.info_ax
+        if hasattr(self, 'info_axes'):
+            for ax in self.info_axes:
+                ax.remove()
+            self.info_axes.clear()
 
     def add_invalid_text(self):
         """ Adds text to the figure to indicate invalid input. """
@@ -368,9 +384,48 @@ class FigureManager:
             del self.invalid_ax  # Clean up the reference
 
 
-    # def plot_point(self, x_val, y_val):
-    #     x_val = int(x_val)
-    #     y_val = int(y_val)
-    #     self.ax.scatter([x_val], [y_val], color='red', s=100, marker='x')  # Mark with red cross
-    #     self.ax.annotate(f'Marked at ({x_val}, {y_val})', (x_val, y_val),
-    #                       textcoords="offset points", xytext=(0,10), ha='center')
+    def plot_point(self, x_val, y_val):
+        """ 
+        Creates a scatter point and annotation on the figure for valid points.
+        
+        Parameters
+        ----------
+        x_val: string
+            The value from the text box that was used in validation
+        y_val: int
+            The value stored in the object array retreived with the x_val
+        
+        """
+        x_val = int(x_val)
+        y_val = int(y_val)
+
+        if not hasattr(self, 'scatter_objects'):
+            self.scatter_objects = []
+
+        if not hasattr(self, "annotation_objects"):
+            self.annotation_objects = []
+
+        #check if we are using display with shared y or not
+        if self.ax2 is not None:
+            scatter = self.ax2.scatter([x_val], [y_val], color='red', s=100, marker='x')  # Mark with red cross
+            annotation = self.ax2.annotate(f'({x_val}, {y_val})', (x_val, y_val),
+                          textcoords="offset points", xytext=(0,10), ha='center')
+        else:
+            scatter = self.ax.scatter([x_val], [y_val], color='red', s=100, marker='x') 
+            annotation = self.ax.annotate(f'({x_val}, {y_val})', (x_val, y_val),
+                          textcoords="offset points", xytext=(0,10), ha='center')
+        self.scatter_objects.append(scatter)
+        self.annotation_objects.append(annotation)
+        
+    def remove_points(self):
+        """
+        Removes all scatter points and annotations created from the plot_point() function
+        """
+        if hasattr(self, 'scatter_objects'):
+            for scatter in self.scatter_objects:
+                scatter.remove()
+            self.scatter_objects.clear()
+        if hasattr(self, 'annotation_objects'):
+            for annotation in self.annotation_objects:
+                annotation.remove()
+            self.annotation_objects.clear()
